f951b74dbd4f0240313db4fa030ab7d37ea5492f
Masum Billah <95751615+masum200119@users.noreply.github.com>
2024-05-24 00:00:15 +0600

    Update FCFS.cpp

--- FCFS.cpp
+++ FCFS.cpp
@@ -1,52 +1,93 @@
-#include <iostream>
+// First Code First Serve (FCFS) with Arrival Time
+// Mounish Sai (https://github.com/pvsmounish)
+
+#include<iostream>
+#include<limits>
 using namespace std;
 
-int main()
-{
-    int bt[20], wt[20], tat[20], ct[20], at[20], i, n;
-    float wtavg = 0, tatavg = 0;
+class Process{
+    public:
+        string processName;
+        int arrivalTime;
+        int burstTime;
 
-    cout << "Enter the number of processes: ";
-    cin >> n;
+        int remainingTime;
 
-    for (i = 0; i < n; i++)
-    {
-        cout << "Enter Arrival Time for Process " << i << ": ";
-        cin >> at[i];
+        int responseTime;
+        int completionTime;
+        
+        int waitingTime;
+        int turnAroundTime;
+};
 
-        cout << "Enter Burst Time for Process " << i << ": ";
-        cin >> bt[i];
+int main(){
+    int numOfProcesses;
+    cout << "Enter no. of processes: ";
+    cin >> numOfProcesses;
+
+    Process processes[numOfProcesses];
+
+    for(int n=0;n<numOfProcesses;n++){
+        cout << "\nEnter Process Name for " << (n+1) << ": ";
+        cin >> processes[n].processName;
+        cout << "Enter Arrival Time for Process " << (n+1) << ": ";
+        cin >> processes[n].arrivalTime;
+        cout << "Enter Burst Time for Process " << (n+1) << ": ";
+        cin >> processes[n].burstTime;
     }
 
-    ct[0] = at[0] + bt[0];
-    tat[0] = ct[0] - at[0];
-    wt[0] = tat[0] - bt[0];
-    wtavg += wt[0];
-    tatavg += tat[0];
+    cout << "\n" << endl;
 
-    for (i = 1; i < n; i++)
-    {
-        // Calculate the completion time based on the arrival time and previous completion time
-        if (at[i] > ct[i - 1])
-            ct[i] = at[i] + bt[i];
-        else
-            ct[i] = ct[i - 1] + bt[i];
-
-        tat[i] = ct[i] - at[i];
-        wt[i] = tat[i] - bt[i];
-
-        wtavg += wt[i];
-        tatavg += tat[i];
+    for(int i=0;i<numOfProcesses-1;i++){
+        for(int j=i+1;j<numOfProcesses;j++){
+            if(processes[j].arrivalTime < processes[i].arrivalTime){
+                Process temp = processes[j];
+                processes[j] = processes[i];
+                processes[i] = temp;
+            }
+        }
     }
 
-    cout << "\nPROCESS \tARRIVAL TIME \tBURST TIME \tCOMPLETION TIME \tWAITING TIME \tTURNAROUND TIME\n";
-    for (i = 0; i < n; i++)
-    {
-        cout << "\n\t P" << i << "\t\t " << at[i] << "\t\t " << bt[i] << "\t\t " << ct[i] << "\t\t " << wt[i] << "\t\t " << tat[i];
+    int sumResponseTime = 0;
+    int sumCompletionTime = 0;
+    int sumWaitingTime = 0;
+    int sumTurnAroundTime = 0;
+  
+    
+    int cumulativeSum = 0;
+
+
+    for(int n=0;n<numOfProcesses;n++){
+        processes[n].waitingTime = cumulativeSum;
+        processes[n].completionTime =  processes[n].waitingTime + processes[n].burstTime;
+        if(n == 0){
+            processes[n].responseTime = 0;
+        }
+        else{
+            processes[n].responseTime = processes[n-1].completionTime;
+        }
+        
+        processes[n].turnAroundTime = processes[n].completionTime - processes[n].arrivalTime;
+
+        cout << "\nProcess " << processes[n].processName << ":\n";
+        cout << "Response Time: " << processes[n].responseTime << endl;
+        cout << "Completion Time: " << processes[n].completionTime << endl;
+        cout << "Waiting Time: " << processes[n].waitingTime << endl;
+        cout << "Turn Around Time: " << processes[n].turnAroundTime << endl;
+
+        sumResponseTime += processes[n].responseTime;
+        sumCompletionTime += processes[n].completionTime;
+        sumWaitingTime += processes[n].waitingTime;
+        sumTurnAroundTime += processes[n].turnAroundTime;
+
+        cumulativeSum += processes[n].burstTime;
     }
 
-    cout << "\nAverage Waiting Time -- " << wtavg / n;
-    cout << "\nAverage Turnaround Time -- " << tatavg / n;
+
+    cout << "\n\nAverage Response Time for " << (numOfProcesses) << " Processes: " << (float) sumResponseTime/numOfProcesses;
+    cout << "\n\nAverage Completion Time for " << (numOfProcesses) << " Processes: " << (float) sumCompletionTime/numOfProcesses;
+    cout << "\n\nAverage Waiting Time for " << (numOfProcesses) << " Processes: " << (float) sumWaitingTime/numOfProcesses;
+    cout << "\n\nAverage Turn Around Time for " << (numOfProcesses) << " Processes: " << (float) sumTurnAroundTime/numOfProcesses;
 
     return 0;
 }
